da449d753a03dca1633bdc9eb28085c0af5eaa49
Tiezzi Junior <marco.tiezzi@sou.unifeob.edu.br>
2025-02-14 13:52:02 -0300

    update index
    Atualizando com os valores e a home

--- landing_page1/index.php
+++ landing_page1/index.php
@@ -8,6 +8,9 @@
 
     <link rel="icon" href="./src/assets/icons/favicon.ico" type="image/x-icon">
 
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
     <link rel="stylesheet" href="./src/style/reset.css">
     <link rel="stylesheet" href="./src/style/vars.css"> <!-- VARIAVEIS DO CSS -->
     <link rel="stylesheet" href="./src/style/topbar-footer.css"> <!-- CSS DO TOPBAR - FOOTER -->
@@ -21,21 +24,58 @@
 
     <main>
         <div class="main-content">
+            <!-- EXIBIÇÃO INICIAL DO SITE -->
             <div class="header__content">
-                <h1 class="header__content-title">Experience<br>/Elevated<br>Living</h1>
-                <p class="header__content-subtitle">Luxurius spaces, refined design, personalized services, and modern</p>
-                <p class="header__content-subtitle2">amenities for a sophisticated, upscale lifestyle.</p>
+                <div class="header__content-header">
+                    <h1 class="header__content-title">Experience<br>/Elevated<br>Living</h1>
+                    <p class="header__content-subtitle">Luxurius spaces, refined design, personalized services, and modern</p>
+                    <p class="header__content-subtitle2">amenities for a sophisticated, upscale lifestyle.</p>
+                </div>
+
+                <div class="header__content-btn">
+                    <button class="header__content-button">
+                        <span>Learn more</span>
+                    </button>
+                </div>
             </div>
 
             <div class="body__content">
+                <!-- SECTION 1 -->
                 <section class="body__content-valores">
                     <div class="valor__card1">
-                        <span>No.1</span>
-                        <span>Research Market</span>
-                        <span>Explore neighborhoods, property types, and prices to find the right real estate investiment opportunities.</span>
+                        <span class="title">No.1</span>
+                        <span class="subtitle">Research Market</span>
+                        <span class="text">Explore neighborhoods, property types, and prices to find the right real estate investiment opportunities.</span>
                     </div>
-                    <div></div>
-                    <div></div>
+                    <div class="valor__card2">
+                        <span class="title">No.2</span>
+                        <span class="subtitle">Secure Financing</span>
+                        <span class="text">Arrange mortgage options and gather necessary funds to ensure a smooth purchasing process.</span>
+                    </div>
+                    <div class="valor__card3">
+                        <span class="title">No.3</span>
+                        <span class="subtitle">Close Deal</span>
+                        <span class="text">Finalize paperwork, negotiate terms, and transfer ownership to complete your real estate purchase.</span>
+                    </div>
+                </section>
+
+                <!-- SECTION 2 -->
+                <section class="body__content-about">
+                    <div class="title">
+                        <span>/</span>
+                        <span>About us</span>
+                    </div>
+                    <div class="subtitle">
+                        <div>
+                            <span></span>
+                            <span><i class="fa-solid fa-arrow-right"></i></span>
+                        </div>
+                        <div>
+                            <span>At Griyo Real Estate, we provide tailored property solutions, expert guidance, and exceptional service to ensure seamless transactions and satisfaction in every real estate deal.</span>
+                        </div>
+                    </div>
+                    <div class="img"></div>
+                    <div class="text"></div>
                 </section>
             </div>
         </div>
